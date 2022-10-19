01321b23542b1f5cc12615c4e6e187cdc3e11392
Sebastijan Grabar <8758580+baso53@users.noreply.github.com>
2022-10-19 14:16:56 +0200

    Initial commit

---
+++ src/main/java/comsysto/talks/demomaven/DemoMavenApplication.java
@@ -0,0 +1,13 @@
+package comsysto.talks.demomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMavenApplication.class, args);
+	}
+
+}
