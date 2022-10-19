e48c433aa75ea78d2f8c1462f03bf94a50f890e6
Sebastijan Grabar <8758580+baso53@users.noreply.github.com>
2022-10-19 19:40:57 +0200

    Comment out real world test

--- src/main/java/comsysto/talks/demojava/TestController.java
+++ src/main/java/comsysto/talks/demojava/TestController.java
@@ -1,35 +1,35 @@
-package comsysto.talks.demojava;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.stream.Stream;
-
-@RequestMapping("test-controller")
-@RestController
-class TestController {
-
-  @PostMapping
-  Object createManifest(@RequestBody TestRequest request) {
-    return switch (request) {
-      case WithInteger s -> s.intValue();
-      case WithString s -> s.stringValue();
-      case WithComplexType(ComplexType(var x, var y)) -> x + y;
-      case WithGenericType<?>(Integer x) -> x * 1000;
-      case WithGenericType<?>(String x) -> x.toUpperCase();
-      case WithGenericType<?>(var x) -> x;
-    };
-  }
-
-  @PostMapping("with-list")
-  Object createManifestWithList(@RequestBody List<TestRequest> request) {
-    return request
-        .stream()
-        .flatMap(req -> Stream.ofNullable(req instanceof WithInteger i ? i : null))
-        .map(WithInteger::intValue)
-        .toList();
-  }
-}
+//package comsysto.talks.demojava;
+//
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//import java.util.stream.Stream;
+//
+//@RequestMapping("test-controller")
+//@RestController
+//class TestController {
+//
+//  @PostMapping
+//  Object createManifest(@RequestBody TestRequest request) {
+//    return switch (request) {
+//      case WithInteger s -> s.intValue();
+//      case WithString s -> s.stringValue();
+//      case WithComplexType(ComplexType(var x, var y)) -> x + y;
+//      case WithGenericType<?>(Integer x) -> x * 1000;
+//      case WithGenericType<?>(String x) -> x.toUpperCase();
+//      case WithGenericType<?>(var x) -> x;
+//    };
+//  }
+//
+//  @PostMapping("with-list")
+//  Object createManifestWithList(@RequestBody List<TestRequest> request) {
+//    return request
+//        .stream()
+//        .flatMap(req -> Stream.ofNullable(req instanceof WithInteger i ? i : null))
+//        .map(WithInteger::intValue)
+//        .toList();
+//  }
+//}
--- src/main/java/comsysto/talks/demojava/TestRequest.java
+++ src/main/java/comsysto/talks/demojava/TestRequest.java
@@ -1,24 +1,24 @@
-package comsysto.talks.demojava;
-
-import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.DEDUCTION;
-
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-
-@JsonTypeInfo(use = DEDUCTION)
-@JsonSubTypes({
-    @Type(WithInteger.class),
-    @Type(WithString.class),
-    @Type(WithComplexType.class),
-    @Type(WithGenericType.class)
-})
-sealed interface TestRequest permits WithInteger, WithString, WithComplexType, WithGenericType { }
-
-record ComplexType(Integer x, Integer y) { }
-
-record WithInteger(Integer intValue) implements TestRequest { }
-record WithString(String stringValue) implements TestRequest { }
-record WithComplexType(ComplexType complexValue) implements TestRequest { }
-record WithGenericType<T>(T genericValue) implements TestRequest { }
+//package comsysto.talks.demojava;
+//
+//import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.DEDUCTION;
+//
+//import com.fasterxml.jackson.annotation.JsonSubTypes;
+//import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+//import com.fasterxml.jackson.annotation.JsonTypeInfo;
+//
+//
+//@JsonTypeInfo(use = DEDUCTION)
+//@JsonSubTypes({
+//    @Type(WithInteger.class),
+//    @Type(WithString.class),
+//    @Type(WithComplexType.class),
+//    @Type(WithGenericType.class)
+//})
+//sealed interface TestRequest permits WithInteger, WithString, WithComplexType, WithGenericType { }
+//
+//record ComplexType(Integer x, Integer y) { }
+//
+//record WithInteger(Integer intValue) implements TestRequest { }
+//record WithString(String stringValue) implements TestRequest { }
+//record WithComplexType(ComplexType complexValue) implements TestRequest { }
+//record WithGenericType<T>(T genericValue) implements TestRequest { }
