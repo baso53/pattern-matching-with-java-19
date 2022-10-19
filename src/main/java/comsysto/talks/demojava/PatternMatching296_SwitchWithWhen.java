47f9ff008ca5e4f1c2c1cf51a4900658d4832692
Sebastijan Grabar <8758580+baso53@users.noreply.github.com>
2022-10-19 20:40:55 +0200

    Make code better

--- src/main/java/comsysto/talks/demojava/PatternMatching296_SwitchWithWhen.java
+++ src/main/java/comsysto/talks/demojava/PatternMatching296_SwitchWithWhen.java
@@ -1,24 +1,25 @@
 //package comsysto.talks.demojava;
 //
-//record IntegerHolder(Integer integerValue) {
-//}
+//record IntegerHolder(Integer integerValue) { }
 //
 //public class PatternMatching296_SwitchWithWhen {
 //
 //  public static void main(String[] args) {
+//    System.out.println(groupNumberBySize(5));
 //    System.out.println(groupNumberBySize(new IntegerHolder(5)));
+//    System.out.println(groupNumberBySize(500));
 //    System.out.println(groupNumberBySize(new IntegerHolder(500)));
 //    System.out.println(groupNumberBySize(new IntegerHolder(50000)));
 //  }
 //
 //  static String groupNumberBySize(Object request) {
 //    return switch (request) {
-//      case Integer i when i < 10 -> "small";
-//      case IntegerHolder(var i) when i < 10 -> "small";
-//      case Integer i when i < 1000 -> "medium";
-//      case IntegerHolder(var i) when i < 10 -> "medium";
-//      case Integer i when i < 100000 -> "big";
-//      case IntegerHolder(var i) when i < 10 -> "big";
+//      case Integer i when i < 10 -> i + " is small";
+//      case IntegerHolder(var i) when i < 10 -> i + " is small";
+//      case Integer i when i < 1000 -> i + " is medium";
+//      case IntegerHolder(var i) when i < 1000 -> i + " is medium";
+//      case Integer i when i >= 100000 -> i + " is big";
+//      case IntegerHolder(var i) when i >= 100000 -> i + " is big";
 //      default -> "neither big or small";
 //    };
 //  }
