1c7bbc672a7ddad09928c95a2326798e1fea8c32
Sebastijan Grabar <8758580+baso53@users.noreply.github.com>
2022-10-19 19:40:33 +0200

    Comment out fun with when

--- src/main/java/comsysto/talks/demojava/PatternMatching298_FunWithWhen.java
+++ src/main/java/comsysto/talks/demojava/PatternMatching298_FunWithWhen.java
@@ -1,23 +1,23 @@
-package comsysto.talks.demojava;
-
-sealed interface Fun permits When {}
-
-record When() implements Fun {
-  public static Boolean when = true;
-}
-
-public class PatternMatching298_FunWithWhen {
-
- public static void main(String[] args) {
-   System.out.println(letsHaveSomeFun(new When()));
- }
-
- static String letsHaveSomeFun(Fun fun) {
-   return switch (fun) {
-     case When when when when.when -> when();
-     default -> "Default";
-   };
- }
-
- static String when() {return "When?";}
-}
+//package comsysto.talks.demojava;
+//
+//sealed interface Fun permits When {}
+//
+//record When() implements Fun {
+//  public static Boolean when = true;
+//}
+//
+//public class PatternMatching298_FunWithWhen {
+//
+// public static void main(String[] args) {
+//   System.out.println(letsHaveSomeFun(new When()));
+// }
+//
+// static String letsHaveSomeFun(Fun fun) {
+//   return switch (fun) {
+//     case When when when when.when -> when();
+//     default -> "Default";
+//   };
+// }
+//
+// static String when() {return "When?";}
+//}
